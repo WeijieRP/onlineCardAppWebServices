50489c5fc946130748f8592677635d0886a4aa8f
weijie <hooiweijie60@gmail.com>
2026-01-06 01:47:09 +0800

    fouth  commit

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ const dbConfig = {
     host: process.env.DB_HOST,
     user:process.env.DB_USER,
     password:process.env.DB_PASSWORD,
-    database:process.env.DB_Name,
+    database:process.env.DB_NAME,
     port:process.env.DB_PORT,
     waitForConnections:true,
     connectionLimit:100,
